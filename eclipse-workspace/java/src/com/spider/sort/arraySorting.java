5c8aa8eb587301f48d8b186152e2e9e18dd926e8
Raushan Kumar Gupta <123105839+raushangupta05@users.noreply.github.com>
2025-01-31 20:55:50 +0530

    Sort the array

---
+++ eclipse-workspace/java/src/com/spider/sort/arraySorting.java
@@ -0,0 +1,19 @@
+package com.spider.sort;
+
+import java.util.Arrays;
+
+public class arraySorting {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 7, 4, 2, 8, 11, 58, 52 };
+
+		Arrays.sort(arr);
+
+		for (int i : arr) {
+
+			System.out.print(i + " ");
+		}
+
+	}
+}
